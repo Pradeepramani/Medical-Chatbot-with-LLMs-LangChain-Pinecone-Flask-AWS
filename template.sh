7103dfe74a9af44a51d90c8aca2b0db679c2ff8d
Pradeep421 <pradeepamsavalli@gmail.com>
2025-07-21 10:37:22 +0530

    first commit

---
+++ template.sh
@@ -0,0 +1,15 @@
+# setup the directory
+mkdir -p src
+mkdir -p research
+
+#creating the file
+touch src/__init__.py 
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirement.txt
+
+echo "all the file has been created"
